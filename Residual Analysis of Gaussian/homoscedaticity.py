c432bde527a170b9fd97e3eedbeeb071e6302cc1
Debashis Saikia <saikiadebashis5@gmail.com>
2026-04-06 10:25:23 +0530

    Implement residual analysis for Gaussian homoscedasticity
    This script performs residual analysis for Gaussian homoscedasticity using a polynomial regression model. It visualizes the residuals against predicted values to assess model fit.

---
+++ Residual Analysis of Gaussian/homoscedaticity.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Gaussian_Analysis2d import PolynomialRegression2D
+import pandas as pd
+import scipy.stats as stats
+
+data = pd.read_csv(r"C:\Users\IISER13\OneDrive\Desktop\PHY 5032\processed_stm_dataset_1.csv")
+
+x = data["x_nm"].values
+y = data["y_nm"].values
+z = data["log_current"].values
+
+x = x[:100000]
+y = y[:100000]
+z = z[:100000]
+
+model = PolynomialRegression2D(degree=5)
+model.fit(x, y, z)
+model.model_info()
+z_pred = model.predict(x, y)
+residuals = z - z_pred
+
+plt.scatter(z_pred, residuals, alpha=0.1)
+plt.axhline(0)
+plt.xlabel("Predicted")
+plt.ylabel("Residuals")
+plt.title("Residual vs Prediction")
+plt.show()
