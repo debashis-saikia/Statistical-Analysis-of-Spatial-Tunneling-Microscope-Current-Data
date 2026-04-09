078b8b94ce0f02d2794c371b577d39a60f58d681
Arya74phy <debanjanchatterjee74@gmail.com>
2026-04-09 19:36:41 +0530

    Update gaussian_vs_poisson_regression.py

--- Bayesian Approach/Bayes factor/gaussian_vs_poisson_regression.py
+++ Bayesian Approach/Bayes factor/gaussian_vs_poisson_regression.py
@@ -1,22 +1,24 @@
 degree = 2
 
-#Gaussian
-model_g = PolynomialRegression2D(degree=degree)
-model_g.fit(x, y, z)
+# Convert to counts
+curr = np.exp(z)
+counts = np.round(100 * curr / np.mean(curr)).astype(int)
+counts[counts < 0] = 0
+curr_counts = counts
 
+# Gaussian likelihood
 logL_g = np.mean([
-    log_likelihood_gaussian(s, x, y, z, model_g.polynomial_model)
+    log_likelihood_gaussian(
+        s, x, y, z, model_g.polynomial_model
+    )
     for s in samples_g[:1000]
 ])
 
-# Poisson
-
-model_p = PolynomialRegression2D(degree=degree)
-
-curr = np.exp(z)
-
+# Poisson likelihood
 logL_p = np.mean([
-    log_likelihood_poisson(s, x, y, curr, model_p.polynomial_model)
+    log_likelihood_poisson(
+        s, x, y, curr_counts, model_p.polynomial_model
+    )
     for s in samples_p[:1000]
 ])
 
